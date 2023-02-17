b4c1cd0dae2858c7e65575166b07fac565d22396
Kundan Jangir <102275609+kundan7742742518@users.noreply.github.com>
2023-02-17 18:05:31 +0530

    Add files via upload

---
+++ Assingment_29.py
@@ -0,0 +1,77 @@
+"""
+ANS = 1
+Guido van Rossum
+
+ANS = 2
+Python is dynamically typed and garbage-collected. 
+It supports multiple programming paradigms, 
+including structured (particularly procedural), 
+object-oriented and functional programming.
+
+ANS = 3
+Yes, Python is a case-sensitive language
+
+ANS = 4
+
+(.py) is the correct extenction oif python file.
+
+ANS = 5
+Python code is interpreted. python is the interpretion language
+
+ANS = 6
+a module, a function body, and a class definition
+
+ANS = 7
+# is used for single line comment 
+
+ANS  = 8
+
+Using sys. version method.
+Using python_version() function.
+Using Python -V command.
+
+ANS = 9
+ lambda. 
+
+ANS = 10
+PIP is a recursive acronym for “Preferred Installer Program” or PIP Installs Packages.
+
+ANS = 11
+list 
+set 
+lanbda
+print
+for loop 
+while loop
+
+ANS = 12
+An identifier can have a maximum length of 79 characters in Python. 
+
+ANS = 13
+pyhton is easy to use 
+python have a huge amount of thired party library
+Data science.
+Scientific and mathematical computing.
+Web development.
+Finance and trading.
+System automation and administration.
+Computer graphics.
+Basic game development.
+Security and penetration testing.
+
+ANS = 14
+Memory management in Python involves a private heap containing all Python objects and data structures. 
+The management of this private heap is ensured internally by the Python memory manager.
+
+ANS = 15
+Right-clicking This PC and going to Properties.
+Clicking on the Advanced system settings in the menu on the left.
+Clicking on the Environment Variables button on the bottom right.
+In the System variables section, selecting the Path variable and clicking on Edit. 
+The next screen will show all the directories that are currently a part of the PATH variable.
+Clicking on New and entering Python's install directory.
+
+ANS = 16
+Yes , indentation is requird
+
+"""
