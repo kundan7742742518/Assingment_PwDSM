b3f96a6bf71b5b5a62b54b94b448a6abbefd6d65
Kundan Jangir <102275609+kundan7742742518@users.noreply.github.com>
2023-04-01 14:12:06 +0530

    Add files via upload

---
+++ assingment_21.py
@@ -0,0 +1,67 @@
+# Assingment no.21
+# ANS= 1
+"""
+Web scraping is the automated process of extracting data from websites using software 
+or scripts. 
+This data can then be analyzed, organized, and used for various purposes. 
+
+Data Collection
+Market research 
+content aggregation 
+
+
+E-commarce 
+social midea 
+job market 
+"""
+
+# ANS = 2
+
+"""
+Manual Scraping
+Web Scraping Tools:-Scrapy, Beautiful Soup, and Selenium
+APIs: Some websites provide APIs (Application Programming Interfaces)
+Browser Extensions:  Examples of browser extensions include Web Scraper, Data Miner, and Scraper.
+"""
+# ANS = 3
+
+"""
+Beautiful Soup is a Python library that is used for web scraping purposes. 
+It is a powerful tool that allows developers to extract data from HTML and XML files. 
+
+Why it is use :-
+Parsing HTML and XML
+East to use 
+Robustness
+Flexibility
+"""
+# ANS = 4
+
+""""
+web interface 
+Scalability
+data storege 
+Integration
+
+"""
+
+# ANS = 5
+
+""""
+Elastic beanstack
+
+
+Amazon EC2 (Elastic Compute Cloud): 
+EC2 is a web service that provides resizable compute capacity in the cloud.
+Amazon S3 (Simple Storage Service): 
+S3 is a storage service that provides object storage in the cloud
+Amazon RDS (Relational Database Service):
+RDS is a managed database service that provides scalable and cost-effective relational databases in the cloud.
+Amazon DynamoDB: 
+DynamoDB is a managed NoSQL database service that provides fast and flexible document and key-value database in the cloud.
+Amazon SNS (Simple Notification Service): 
+SNS is a messaging service that provides reliable and scalable message delivery in the cloud. I
+
+
+
+"""
